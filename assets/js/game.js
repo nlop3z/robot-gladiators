02175f212e7a8b20177cd9c084795bcc14b72eba
nlopez <nelly.lopez@utexas.edu>
2020-10-11 20:54:00 -0500

    added getPlayerName fx to fix null and blank bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -66,7 +66,7 @@ var fight = function (enemy) {
       if (playerInfo.health > 0) {
         // let user know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
         window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-        debugger;
+        //debugger;
         // pick new enemy to fight based on the index of the enemyNames array
         var pickedEnemyObj = enemyInfo[i];
         // reset enemy.health before starting new fight
@@ -143,9 +143,18 @@ var fight = function (enemy) {
 
     return value;
   };
+  // function to set name
+var getPlayerName = function() {
+  var name = "";
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  //console.log("" Your robot's name is "" + name);
+  return name;
+};
   //player info object
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
